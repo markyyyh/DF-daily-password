f774e3523bce6ec70a3364b6a81c6190c2dc09a4
markyyyh <uaremarkyang@gmail.com>
2026-07-10 21:30:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,18 @@ import requests
 URL = "https://www.playdeltaforce.com/events/hq/zh-tw/m/index.html"
 
 response = requests.get(URL)
-
 response.encoding = "utf-8"
 
 html = response.text
 
 
-print("HTML 長度：", len(html))
+for word in ["password", "code", "零號大壩"]:
 
-# 找可能存資料的位置
-for word in [
-    "0213",
-    "0911",
-    "零號大壩",
-    "password",
-    "passwords",
-    "code",
-    "api"
-]:
-    print("\n搜尋：", word)
+    print("\n==========", word, "==========")
 
-    if word in html:
-        print("找到！")
+    index = html.find(word)
+
+    if index != -1:
+        print(html[index-500:index+500])
     else:
-        print("沒有")
+        print("找不到")
