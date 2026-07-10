3766b767c4053e67df6fd3abbfbc2ff11a96697e
markyyyh <uaremarkyang@gmail.com>
2026-07-10 20:51:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,53 @@
 import os
 import requests
+from bs4 import BeautifulSoup
 from datetime import datetime
 
-# 從 GitHub Secret 讀取 Discord Webhook
+
 WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-today = datetime.now().strftime("%Y-%m-%d")
+URL = "https://www.playdeltaforce.com/events/hq/zh-tw/m/index.html"
+
+
+def get_page_text():
+
+    response = requests.get(URL)
+
+    response.encoding = "utf-8"
+
+    soup = BeautifulSoup(
+        response.text,
+        "html.parser"
+    )
+
+    return soup.get_text(
+        "\n"
+    )
+
+
+text = get_page_text()
+
+
+today = datetime.now().strftime(
+    "%Y-%m-%d"
+)
+
 
 message = f"""△Daily password
 
 日期：{today}
 
-零號大壩：測試
-長弓溪谷：測試
-巴克什：測試
-航天基地：測試
-潮汐監獄：測試
-AZ3：測試
+網頁讀取測試：
+
+{text[:1000]}
 
 Hope every1 got red until u rest in peace
 """
 
-response = requests.post(
-    WEBHOOK,
-    json={"content": message}
-)
 
-print(response.status_code)
+requests.post(
+    WEBHOOK,
+    json={
+        "content": message
+    }
+)
