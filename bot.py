ca72e1995977666e2e62e324c144494262fe6ed1
markyyyh <uaremarkyang@gmail.com>
2026-07-11 09:14:03 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
 
 
 URL = "https://www.playdeltaforce.com/events/hq/en/m/index.html"
@@ -8,57 +6,24 @@ URL = "https://www.playdeltaforce.com/events/hq/en/m/index.html"
 
 html = requests.get(URL).text
 
-soup = BeautifulSoup(html, "html.parser")
+
+print("HTML長度:", len(html))
 
 
-scripts = []
-
-for script in soup.find_all("script"):
-
-    src = script.get("src")
-
-    if src:
-        scripts.append(urljoin(URL, src))
-
-
-print("JS 數量:", len(scripts))
-
-
-keywords = [
-    "operations",
-    "password",
-    "6905",
+for word in [
     "2119",
-    "daily",
-    "code",
-    "fetch",
-    "ajax",
-    "xmlhttprequest"
-]
+    "6905",
+    "operations-zero-dam",
+    "operations-layali-grove",
+    "Daily Password",
+    "password"
+]:
 
+    print("\n搜尋:", word)
 
-for js_url in scripts:
+    i = html.find(word)
 
-    try:
-        js = requests.get(js_url).text
-
-        print("\n==========")
-        print(js_url)
-        print("長度:", len(js))
-
-
-        for word in keywords:
-
-            if word.lower() in js.lower():
-
-                print("找到:", word)
-
-                index = js.lower().find(word.lower())
-
-                print(
-                    js[index-200:index+500]
-                )
-
-    except Exception as e:
-
-        print("錯誤:", e)
+    if i != -1:
+        print(html[i-300:i+500])
+    else:
+        print("沒有")
