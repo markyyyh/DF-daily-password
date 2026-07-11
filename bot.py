ded5998ad4ade8d66f9bd516b85495e9112e4b6c
markyyyh <uaremarkyang@gmail.com>
2026-07-11 09:21:18 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import time
 
+url = "https://www.playdeltaforce.com/events/hq/assets/m/index.html-BT5wd2G1.js"
 
-URL = "https://www.playdeltaforce.com/events/hq/en/m/index.html?t=" + str(int(time.time()))
+js = requests.get(url).text
 
-html = requests.get(
-    URL,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    }
-).text
+print(js)
 
-
-soup = BeautifulSoup(html, "html.parser")
-
-
-for script in soup.find_all("script"):
-
-    src = script.get("src")
-
-    if src:
-
-        js_url = urljoin(URL, src)
-
-        print("\nJS:", js_url)
-
-        js = requests.get(
-            js_url,
-            headers={
-                "User-Agent": "Mozilla/5.0"
-            }
-        ).text
-
-
-        for word in [
-            "operations-zero-dam",
-            "setData",
-            "data-info",
-            "ajax",
-            "get"
-        ]:
-
-            if word in js:
-                print("找到:", word)
+print("\n長度:", len(js))
