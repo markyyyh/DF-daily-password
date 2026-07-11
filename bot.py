b776a090d4cdd7853617ffb41dd4940f8b278edf
markyyyh <uaremarkyang@gmail.com>
2026-07-11 09:24:04 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,48 @@
 import requests
 
-url = "https://www.playdeltaforce.com/events/hq/assets/m/index.html-BT5wd2G1.js"
 
-js = requests.get(url).text
+base = "https://www.playdeltaforce.com/events/hq/assets/m/"
 
-print(js)
 
-print("\n長度:", len(js))
+files = [
+    "wand-Dnsle5-v.js",
+    "main-DYD4Ogaa.js",
+    "keywords-BY-fuJl3.js",
+    "convert-DkzLyfDU.js",
+    "timeServerPlugin-6YcmO4yg.js"
+]
+
+
+words = [
+    "6905",
+    "2119",
+    "operations-zero-dam",
+    "password",
+    "daily",
+    "api",
+    "fetch",
+    "axios",
+    "request"
+]
+
+
+for file in files:
+
+    url = base + file
+
+    js = requests.get(url).text
+
+    print("\n================")
+    print(file)
+    print("長度:", len(js))
+
+
+    for word in words:
+
+        if word.lower() in js.lower():
+
+            print("找到:", word)
+
+            i = js.lower().find(word.lower())
+
+            print(js[i-300:i+500])
